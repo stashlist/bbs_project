f94794b9c504f032e6460a46b873637b50997fe5
stashlist <azs.15052@gmail.com>
2025-02-26 10:46:15 +0900

    環境変数の設定 & データベース設定を変更

--- bbs_project/settings.py
+++ bbs_project/settings.py
@@ -11,21 +11,33 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
+from dotenv import load_dotenv
+
+# .env を読み込む
+load_dotenv()
+
+# 環境変数を取得
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+DEBUG = os.getenv("DEBUG", "False") == "True"
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+
+# DATABASE 設定
+import dj_database_url
+DATABASES = {
+    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+# SECRET_KEY = 'django-insecure-@#104$8e1^oztiyvu#z5_9)m^b08c0!h)=+%^9x3@3ai9g+y*d'
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@#104$8e1^oztiyvu#z5_9)m^b08c0!h)=+%^9x3@3ai9g+y*d'
+# DEBUG = True
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# ALLOWED_HOSTS = ["192.168.1.10", "your-public-ip", "example.com", "*"]  # "*" はすべて許可（危険）
 
-ALLOWED_HOSTS = ["192.168.1.10", "your-public-ip", "example.com", "*"]  # "*" はすべて許可（危険）
 
 
 AUTH_USER_MODEL = 'board.CustomUser'
@@ -109,16 +121,16 @@ WSGI_APPLICATION = 'bbs_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bbs_db',
-        'USER': 'bbs_user',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'bbs_db',
+#         'USER': 'bbs_user',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
